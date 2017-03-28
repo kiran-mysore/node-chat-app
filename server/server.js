57988e472185c6d8d287a95ba027aa9e5254c952
kiran-mysore <kiran.mysore.rao@gmail.com>
2017-03-28 16:02:58 +0200

    Welcome new users and alert others

--- server/server.js
+++ server/server.js
@@ -38,10 +38,28 @@ io.on('connection',(socket)=>{
         createAt:new Date()
     })*/
 
+
+    socket.emit('newMessage',{
+        from:'Admin',
+        text:'Welcome to the chat app'
+    })
+
+    socket.broadcast.emit('newMessage',{
+        from:'Admin',
+        text:'New user joined',
+        createdAt:new Date().getTime()
+    })
     socket.on('createMessage',(newMessage)=>{
         console.log('New message from the client',newMessage)
         // Emit this new message to all the users connected
-        io.emit('newMessage',{
+  /*      io.emit('newMessage',{
+            from:newMessage.from,
+            text:newMessage.text,
+            createdAt:new Date().getTime()
+        })*/
+
+        //Emit this message to the users connected except the sender
+        socket.broadcast.emit('newMessage',{
             from:newMessage.from,
             text:newMessage.text,
             createdAt:new Date().getTime()
