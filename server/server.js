79978961198a1da7e8dd6d560989451c24c3a46c
kiran-mysore <kiran.mysore.rao@gmail.com>
2017-03-28 12:31:48 +0200

    Add connect and disconnect handlers of scoket.io

--- server/server.js
+++ server/server.js
@@ -1,15 +1,31 @@
 const path = require('path') // Nodejs built-in module
 const express = require('express')
+const http = require('http')
+const socketIO = require('socket.io')
 
-const app = express()
-const port  = process.env.PORT || 3000
 //console.log(__dirname + '/../public')
 
 const publicPath = path.join(__dirname, '../public')
 //console.log(publicPath)
 
+const port  = process.env.PORT || 3000
+const app = express()
+const server = http.createServer(app)
+const io = socketIO(server)
+
 // Configure the static folder
 app.use(express.static(publicPath))
-app.listen(port,()=>{
+
+// Register a event listener
+io.on('connection',(socket)=>{
+    console.log("New user connected")
+
+    socket.on('disconnect',()=>{
+        console.log('Client got disconnected')
+    })
+})
+
+
+server.listen(port,()=>{
     console.log(`Chat app listening at port : ${port}`)
 })
